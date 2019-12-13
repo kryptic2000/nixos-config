2011ff3f2fae2b16819506361c81e8b7d8bfced3
adam <adam@persbrandt.emtorp.se>
2019-12-13 14:34:39 +0100

    update base.nix

--- config/base.nix
+++ config/base.nix
@@ -1,6 +1,9 @@
 { config, lib, pkgs, ... }:
 
 {
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+
   boot.kernelParams = ["console=ttyS0,115200n8"];
   boot.loader.grub.extraConfig = ''
     serial --speed=115200 --unit=0 --word=8 --parity=no --stop=1;
@@ -8,7 +11,14 @@
     terminal_output serial
   '';
 
+  networking.useDHCP = false;
+  networking.firewall.enable = true;
+  networking.nameservers = [ "91.228.90.132" ];
+
+
   services.openssh.enable = true;
+  networking.firewall.allowedTCPPorts = [ 22 ];
+
   security.sudo.wheelNeedsPassword = false;
 
 }
