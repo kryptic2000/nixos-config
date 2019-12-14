a061e97b1d8a01761e36920e9461a0add74a6907
System administrator <root@dns04.emtorp.net>
2019-12-14 22:58:59 +0100

    created install script

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+usage() {
+ echo "Usage: $0 <hostname>"
+ exit 1
+}
+
+if [ $# -ne 1 ] ; then
+    usage
+else
+    cd /etc/nixos/
+    pat='^([A-Za-z0-9]+)\.(.*)'
+    [[ "$1" =~ $pat ]]
+    echo HOST="${BASH_REMATCH[1]}"
+    echo DOMAIN="${BASH_REMATCH[2]}"
+    echo "sudo ln -sr /etc/nixos/nixos-config/servers/${BASH_REMATCH[2]}/${BASH_REMATCH[1]}.nix configuration.nix"
+
+fi
