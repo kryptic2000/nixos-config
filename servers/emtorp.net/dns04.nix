244667e0dc2c560cd56c8a7da2a3292e6782b976
System administrator <root@dns04.emtorp.net>
2019-12-14 22:10:11 +0100

    enable bind

--- servers/emtorp.net/dns04.nix
+++ servers/emtorp.net/dns04.nix
@@ -6,15 +6,16 @@
       ../../../hardware-configuration.nix
       ../../config/base.nix
       ../../config/users.nix
-      ../../config/emtorp.net/mx_relay.nix
+      ../../config/dns_resolver.nix
+      #../../config/emtorp.net/mx_relay.nix
     ];
 
   # Use the GRUB 2 boot loader.
   boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
 
-  networking.hostName = "mx01.emtorp.net";
+  networking.hostName = "dns04.emtorp.net";
   networking.interfaces.enp0s3.ipv4.addresses = [ {
-    address = "91.228.90.88";
+    address = "91.228.90.93";
     prefixLength = 28;
   } ];
   networking.defaultGateway = "91.228.90.81";
