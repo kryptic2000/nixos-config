58674a80ba489b3d8cc238caac5eb0f290a29f88
adam <adam@persbrandt.emtorp.se>
2019-12-18 10:52:04 +0100

    added secondary resolver

--- config/base.nix
+++ config/base.nix
@@ -22,7 +22,7 @@
 
   networking.useDHCP = false;
   networking.firewall.enable = true;
-  networking.nameservers = [ "2001:67c:22fc:100::93" ];
+  networking.nameservers = [ "2001:67c:22fc:100::93" "2001:67c:22fc:1::133"];
   networking = {
     timeServers = [
       "sth1.ntp.se"
