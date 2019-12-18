1ba31bdc5c542054989dff57990754e963f5da38
Adam Emtorp <adam@emtorp.se>
2019-12-18 07:37:18 +0000

    fixed setup script

--- install.sh
+++ install.sh
@@ -8,11 +8,11 @@ if [ $# -ne 1 ] ; then
     usage
 else
     cd /etc/nixos/
-    echo "rm configuration.nix"
+    rm configuration.nix
     pat='^([A-Za-z0-9]+)\.(.*)'
     [[ "$1" =~ $pat ]]
     echo HOST="${BASH_REMATCH[1]}"
     echo DOMAIN="${BASH_REMATCH[2]}"
-    echo "sudo ln -sr /etc/nixos/nixos-config/servers/${BASH_REMATCH[2]}/${BASH_REMATCH[1]}.nix configuration.nix"
-
+    n -sr /etc/nixos/nixos-config/servers/${BASH_REMATCH[2]}/${BASH_REMATCH[1]}.nix configuration.nix
+    nixos-rebuild switch --upgrade
 fi
--- servers/emtorp.net/dns05.nix
+++ servers/emtorp.net/dns05.nix
@@ -13,16 +13,16 @@
   boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
 
   networking.hostName = "dns05.emtorp.net";
-  networking.interfaces.enp0s3.ipv4.addresses = [ {
-    address = "91.228.90.";
+  networking.interfaces.ens32.ipv4.addresses = [ {
+    address = "91.228.90.133";
     prefixLength = 28;
   } ];
-  networking.interfaces.enp0s3.ipv6.addresses = [ {
-    address = "2001:67c:22fc:100::";
+  networking.interfaces.ens32.ipv6.addresses = [ {
+    address = "2001:67c:22fc:1::133";
     prefixLength = 64;
   } ];
-  networking.defaultGateway = "91.228.90.81";
-  networking.defaultGateway6 = "2001:67c:22fc:100::1";
+  networking.defaultGateway = "91.228.90.129";
+  networking.defaultGateway6 = "2001:67c:22fc:1::1";
 
   networking.nameservers = [ "8.8.4.4" "8.8.8.8" ];
 
