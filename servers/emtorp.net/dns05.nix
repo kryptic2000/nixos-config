abc2c7b6db07bada26f6018a91905557e1a0bd1f
adam <adam@persbrandt.emtorp.se>
2019-12-19 15:38:38 +0100

    machine updated to new netmodule

--- servers/emtorp.net/dns05.nix
+++ servers/emtorp.net/dns05.nix
@@ -6,24 +6,22 @@
       ../../../hardware-configuration.nix
       ../../config/base.nix
       ../../config/users.nix
+      ../../services/networking.nix
       ../../config/dns_resolver_unbound.nix
     ];
 
   # Use the GRUB 2 boot loader.
   boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
 
-  networking.hostName = "dns05.emtorp.net";
-  networking.interfaces.ens32.ipv4.addresses = [ {
-    address = "91.228.90.133";
-    prefixLength = 28;
-  } ];
-  networking.interfaces.ens32.ipv6.addresses = [ {
-    address = "2001:67c:22fc:1::133";
-    prefixLength = 64;
-  } ];
-  networking.defaultGateway = "91.228.90.129";
-  networking.defaultGateway6 = "2001:67c:22fc:1::1";
 
-  networking.nameservers = [ "8.8.4.4" "8.8.8.8" ];
+  netcfg.ip4 = "91.228.90.133";
+  netcfg.gw4 = "91.228.90.129";
 
+  netcfg.ip6 = "2001:67c:22fc:1::133";
+  netcfg.gw6 = "2001:67c:22fc:1::1";
+
+  netcfg.iface = "ens32";
+  netcfg.hostName = "dns05.emtorp.net";
+
+  netcfg.ns = [ "8.8.4.4" "8.8.8.8" ];
 }
