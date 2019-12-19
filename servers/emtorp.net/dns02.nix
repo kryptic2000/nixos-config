b85e040ad2001bf21cbf760469ad9b06fa4fc823
System administrator <root@dns02.emtorp.net>
2019-12-19 12:58:58 +0100

    network carve out

--- servers/emtorp.net/dns02.nix
+++ servers/emtorp.net/dns02.nix
@@ -6,6 +6,7 @@
       ../../../hardware-configuration.nix
       ../../config/base.nix
       ../../config/users.nix
+      ../../services/networking.nix
       ../../services/dns_slave.nix
       ../../config/emtorp.net/ns1_zones.nix
       ../../services/bgp_vip.nix
@@ -14,39 +15,17 @@
   # Use the GRUB 2 boot loader.
   boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
 
-  netcfg.ip4 = "91.228.90.136";
+  netcfg.ip4 = "91.228.90.140";
   netcfg.gw4 = "91.228.90.129";
 
   netcfg.ip6 = "2001:67c:22fc:1::140";
   netcfg.gw6 = "2001:67c:22fc:1::1";
 
   netcfg.vip4 = "91.228.90.35";
-  netcfg.vip6 = "2001:67c:22fc:5::1";
+  netcfg.vip6 = "2001:67c:22fc:250::1";
 
-  networking.hostName = "dns02.emtorp.net";
-  networking.enableIPv6 = true;
-
-  networking.interfaces.ens32.ipv4.addresses = [ {
-    address = netcfg.ip4;
-    prefixLength = 28;
-  } ];
-  networking.interfaces.ens32.ipv6.addresses = [ {
-    address = netcfg.ip6;
-    prefixLength = 64;
-  } ];
-  networking.interfaces.lo.ipv4.addresses = [ {
-        address = netcfg.vip4;
-        prefixLength = 32;
-  } ];
-  networking.interfaces.lo.ipv6.addresses = [ {
-    address = netcfg.vip6;
-    prefixLength = 128;
-  } ];
-
-  networking.defaultGateway = netcfg.gw4;
-  networking.defaultGateway6 = netcfg.gw6;
-
-  networking.nameservers = [ "8.8.4.4" "8.8.8.8" ];
+  netcfg.iface = "ens32";
+  netcfg.hostName = "dns02.emtorp.net";
 
   environment = {
     systemPackages = with pkgs; [
