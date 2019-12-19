ca628a3ac73275dde378f331f3d1be2787e77588
adam <adam@persbrandt.emtorp.se>
2019-12-19 15:53:27 +0100

    new network module

--- servers/emtorp.net/dns04.nix
+++ servers/emtorp.net/dns04.nix
@@ -6,24 +6,22 @@
       ../../../hardware-configuration.nix
       ../../config/base.nix
       ../../config/users.nix
+      ../../services/networking.nix
       ../../config/dns_resolver_unbound.nix
     ];
 
   # Use the GRUB 2 boot loader.
   boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
 
-  networking.hostName = "dns04.emtorp.net";
-  networking.interfaces.enp0s3.ipv4.addresses = [ {
-    address = "91.228.90.93";
-    prefixLength = 28;
-  } ];
-  networking.interfaces.enp0s3.ipv6.addresses = [ {
-    address = "2001:67c:22fc:100::93";
-    prefixLength = 64;
-  } ];
-  networking.defaultGateway = "91.228.90.81";
-  networking.defaultGateway6 = "2001:67c:22fc:100::1";
+  netcfg.ip4 = "91.228.90.93";
+  netcfg.gw4 = "91.228.90.81";
 
-  networking.nameservers = [ "8.8.4.4" "8.8.8.8" ];
+  netcfg.ip6 = "2001:67c:22fc:1::93";
+  netcfg.gw6 = "2001:67c:22fc:1::1";
+
+  netcfg.iface = "enp0s3";
+  netcfg.hostName = "dns04.emtorp.net";
+
+  netcfg.ns = [ "8.8.4.4" "8.8.8.8" ];
 
 }
