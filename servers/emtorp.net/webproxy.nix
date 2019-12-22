4c73b44682eb247bdae63a8264c06295fb25434c
adam <adam@persbrandt.emtorp.se>
2019-12-22 20:49:21 +0100

    server created

---
+++ servers/emtorp.net/webproxy.nix
@@ -0,0 +1,25 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ../../../hardware-configuration.nix
+      ../../config/base.nix
+      ../../config/users.nix
+      ../../services/networking.nix
+    ];
+
+  # Use the GRUB 2 boot loader.
+  boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
+
+
+  netcfg.ip4 = "91.228.90.139";
+  netcfg.gw4 = "91.228.90.129";
+
+  netcfg.ip6 = "2001:67c:22fc:1::139";
+  netcfg.gw6 = "2001:67c:22fc:1::1";
+
+  netcfg.iface = "ens32";
+  netcfg.hostName = "webproxy.emtorp.net";
+}
+
