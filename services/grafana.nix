f9913dba8060186126c6419cbc7da1897736b318
adam <adam@persbrandt.emtorp.se>
2019-12-24 21:01:43 +0100

    open-fw

--- services/grafana.nix
+++ services/grafana.nix
@@ -1,5 +1,7 @@
 { config, lib, pkgs, netcfg, ... }:
+
 {
+  networking.firewall.allowedTCPPorts = [ 3000 ];
   services.grafana = {
 	addr = ""; # listen (bind) to all network interfaces (i.e. 127.0.0.1, and ipAddress)
 	enable = true;
