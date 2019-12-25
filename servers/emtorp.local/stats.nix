62e2e6fe062b12d2d034b6e50f8ced7842ffd204
System administrator <root@192.168.1.3>
2019-12-25 11:12:38 +0100

    changed networking

--- servers/emtorp.local/stats.nix
+++ servers/emtorp.local/stats.nix
@@ -14,10 +14,10 @@
   # Use the GRUB 2 boot loader.
   boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
 
-  netcfg.ip4 = "192.168.1.7";
+  netcfg.ip4 = "192.168.1.7/24";
   netcfg.gw4 = "192.168.1.1";
   networking.enableIPv6 = true;
-  netcfg.ip6 = "fd00::7";
+  netcfg.ip6 = "fd00::7/128";
 
   netcfg.iface = "enp0s3";
   netcfg.hostName = "192.168.1.3";
--- services/networking.nix
+++ services/networking.nix
@@ -1,18 +1,18 @@
 { config, lib, pkgs, netcfg, ... }:
+let
+  pfx4 = lib.splitString "/" netcfg.ip4;
+  addr4 = { address = lib.elemAt pfx4 0; prefixLength = lib.toInt (lib.elemAt pfx4 1); };
 
+  pfx6 = lib.splitString "/" netcfg.ip6;
+  addr6 = { address = lib.elemAt pfx6 0; prefixLength = lib.toInt (lib.elemAt pfx6 1); };
+in
 {
   networking.hostName = netcfg.hostName;
   networking.enableIPv6 = true;
 
   networking.interfaces.${netcfg.iface} = {
-    ipv4.addresses = [ {
-      address = netcfg.ip4;
-      prefixLength = 28;
-    } ];
-    ipv6.addresses = [ {
-      address = netcfg.ip6;
-      prefixLength = 64;
-    } ];
+    ipv4.addresses = [ addr4 ];
+    ipv6.addresses = [ addr6 ];
   };
 
   networking.defaultGateway = netcfg.gw4;
