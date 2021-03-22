12212fccdd68e1080bf8d61cb6c44d8b9fde4690
adam <adam@persbrandt.emtorp.se>
2021-03-22 15:02:35 +0100

    newrel

---
+++ config/base20.nix
@@ -0,0 +1,74 @@
+{ config, lib, pkgs, netcfg, ... }:
+
+{
+  imports = [
+      ../modules/variables.nix
+  ];
+  boot.loader.grub.enable = true;
+  boot.loader.grub.version = 2;
+
+  boot.kernelParams = ["console=ttyS0,115200n8"];
+  boot.loader.grub.extraConfig = ''
+    serial --speed=115200 --unit=0 --word=8 --parity=no --stop=1;
+    terminal_input serial;
+    terminal_output serial
+  '';
+
+  environment = {
+    systemPackages = with pkgs; [
+      #System tools
+      git
+      wget
+      telnet
+    ];
+  };
+
+
+  networking.useDHCP = false;
+  networking.firewall.enable = true;
+  networking = {
+    timeServers = [
+      "sth1.ntp.se"
+      "sth2.ntp.se"
+      "ntp3.sptime.se"
+      "ntp4.sptime.se"
+      ];
+    };
+
+  networking.firewall.extraCommands = ''
+	iptables -A nixos-fw -p tcp -m state --state NEW --source 91.228.90.0/24 --destination ${netcfg.ip4} --dport 22 -j ACCEPT
+	ip6tables -A nixos-fw -p tcp -m state --state NEW --source 2001:67c:22fc:1::/64 --destination ${netcfg.ip6} --dport 22 -j ACCEPT
+	ip6tables -A nixos-fw -p tcp -m state --state NEW --source 2001:67c:22fc:100::/64 --destination ${netcfg.ip6} --dport 22 -j ACCEPT
+	'';
+
+  services = {
+    openssh = {
+      enable = true;
+      permitRootLogin = "no";
+      passwordAuthentication = false;
+      extraConfig = "MaxAuthTries 1";
+      openFirewall = false;
+    };
+  };
+
+  security.sudo.wheelNeedsPassword = false;
+
+  time.timeZone = "Europe/Stockholm";
+
+  nix = {
+    autoOptimiseStore = true;
+    gc = {
+      automatic = true;
+      dates = "03:30";
+      options = "--delete-older-than 90d";
+    };
+    optimise = {
+      automatic = true;
+      dates = ["04:00"];
+    };
+  };
+
+  system.stateVersion = "20.09";
+
+}
+
