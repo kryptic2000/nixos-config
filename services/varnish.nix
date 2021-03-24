459d758b1dac640c04841af59f984396dd67c547
adam <adam@persbrandt.emtorp.se>
2021-03-24 13:35:54 +0100

    test

--- services/varnish.nix
+++ services/varnish.nix
@@ -4,134 +4,147 @@
   services.varnish.enable = true;
   services.varnish.http_address = "127.0.0.1:6081";
   services.varnish.config = ''
-# This is a basic VCL configuration file for varnish.  See the vcl(7)
-# man page for details on VCL syntax and semantics.
-#
-# Default backend definition.  Set this to point to your content
-# server.
-#
+vcl 4.0;
+
+# Default backend definition. Set this to point to your content server.
 backend default {
     .host = "10.5.1.20";
     .port = "80";
 }
-#
-# Below is a commented-out copy of the default VCL logic.  If you
-# redefine any of these subroutines, the built-in logic will be
-# appended to your code.
-#
 
-sub vcl_recv {
-     set req.grace = 1m;
-     unset req.http.Cookie;
-     unset req.http.Authorization;
-
-     if (req.http.x-forwarded-for) {
-        set req.http.X-Forwarded-For = req.http.X-Forwarded-For ", " client.ip;
-    } else {
-        set req.http.X-Forwarded-For = client.ip;
-  }
-
-
-    if (req.request != "GET" &&
-      req.request != "HEAD" &&
-      req.request != "PUT" &&
-      req.request != "POST" &&
-      req.request != "TRACE" &&
-      req.request != "OPTIONS" &&
-      req.request != "DELETE") {
-        #/* Non-RFC2616 or CONNECT which is weird. */
-        return (pipe);
-    }
-    if (req.request != "GET" && req.request != "HEAD") {
-        #/* We only deal with GET and HEAD by default */
-        return (pass);
-    }
-    if (req.http.Authorization || req.http.Cookie) {
-        #/* Not cacheable by default */
-
-    }
-    if(req.http.X-No-Cache ~ "YES") {
-      return(pass);
-   }
-
-  if (req.http.Accept-Encoding) {
-    if (req.http.Accept-Encoding ~ "gzip") {
-      set req.http.Accept-Encoding = "gzip";
-    } elsif (req.http.Accept-Encoding ~ "deflate") {
-      set req.http.Accept-Encoding = "deflate";
-    } else {
-      remove req.http.Accept-Encoding;
-    }
-  }
-
-  return (lookup);
+acl purgers {
+    "localhost";
+    "127.0.0.1";
 }
 
-sub vcl_fetch {
+sub vcl_recv {
 
-    if (beresp.http.Cache-Control ~ "(no-cache|no-store|private|must-revalidate)") {
-     return(pass);
+    # Allow purging
+    if (req.method == "PURGE") {
+      if (!client.ip ~ purgers) {
+        return (synth(405, "This IP is not allowed to send PURGE requests."));
+      }
+      return (purge);
     }
-    if(beresp.status >= 300){
-        return (pass);
+
+    # Set proxied ip header to original remote address
+    unset req.http.X-Forwarded-For;
+    set req.http.X-Forwarded-For = client.ip;
+
+    # Remove has_js and Google Analytics cookies
+    set req.http.Cookie = regsuball(req.http.Cookie, "(^|;\s*)(__[a-z]+|__utm*|has_js)=[^;]*", "");
+
+    # Remove a ";" prefix, if present.
+    set req.http.Cookie = regsub(req.http.Cookie, "^;\s*", "");
+
+    # Remove empty cookies.
+    if (req.http.Cookie ~ "^\s*$") {
+            unset req.http.Cookie;
     }
-    if (beresp.ttl < 180s){
-        set beresp.ttl = 180s;
+
+    # remove double // in urls,
+        set req.url = regsuball( req.url, "//", "/"      );
+
+    # Normalize Accept-Encoding
+    if (req.http.Accept-Encoding) {
+        if (req.url ~ "\.(jpg|png|gif|gz|tgz|bz2|tbz|mp3|ogg)$") {
+            # No point in compressing these
+            unset req.http.Accept-Encoding;
+        } elsif (req.http.Accept-Encoding ~ "gzip") {
+            set req.http.Accept-Encoding = "gzip";
+        } elsif (req.http.Accept-Encoding ~ "deflate") {
+            set req.http.Accept-Encoding = "deflate";
+        } else {
+            # unknown algorithm
+            unset req.http.Accept-Encoding;
+        }
     }
-    set beresp.grace = 1m;
-  remove beresp.http.Via;
-  remove req.http.cookie;
-  remove beresp.http.Set-Cookie;
-    if (req.url ~ "\.(css|js|jpg|jpeg|gif|ico|png)$") {
-        unset beresp.http.expires;
-        set beresp.http.Cache-Control = "public, max-age=300";
-  set beresp.http.age = "0";
-  set beresp.http.expires = beresp.ttl;
+
+    # Remove cookies for static files
+    if (req.url ~ "\.(gif|jpg|jpeg|swf|css|js|flv|mp3|mp4|pdf|ico|png|tif|tiff|mp3|htm|html|md5)(\?.*|)$") {
+        unset req.http.cookie;
+        return(hash);
+    }
+
+    # Disable caching for backend parts
+    if ( req.url ~ "^/(admin|login)" ) {
+        return(pass);
+    }
+
+    # Strip cookies for cached content
+    unset req.http.Cookie;
+    return(hash);
+
+}
+
+sub vcl_backend_response {
+
+    # If the backend fails, keep serving out of the cache for 30m
+    set beresp.grace = 30m;
+    set beresp.ttl = 48h;
+
+    # Remove some unwanted headers
+    unset beresp.http.Server;
+    unset beresp.http.X-Powered-By;
+
+    # Respect the Cache-Control=private header from the backend
+    if (beresp.http.Cache-Control ~ "private") {
+        set beresp.http.X-Cacheable = "NO:Cache-Control=private";
+    } elsif (beresp.ttl < 1s) {
+        set beresp.ttl   = 120s;
+        set beresp.grace = 5s;
+        set beresp.http.X-Cacheable = "YES:FORCED";
     } else {
-        # set beresp.http.cache-control = "private, max-age=0, must-revalidate";
-        # set beresp.http.pragma = "no-cache";
+        set beresp.http.X-Cacheable = "YES";
     }
-  # only cache successful requests
+
+    # Don't cache responses to posted requests or requests with basic auth
+    if ( bereq.method == "POST" || bereq.http.Authorization ) {
+        set beresp.uncacheable = true;
+        set beresp.ttl = 120s;
+        return (deliver);
+    }
+
+    # Cache error pages for a short while
+    if( beresp.status == 404 || beresp.status == 500 || beresp.status == 301 || beresp.status == 302 ){
+        set beresp.ttl = 1m;
+        return(deliver);
+    }
+
+    # Do not cache non-success response
+    if( beresp.status != 200 ){
+        set beresp.uncacheable = true;
+        set beresp.ttl = 120s;
+        return(deliver);
+    }
+
+    # Strip cookies before these filetypes are inserted into the cache
+    if (bereq.url ~ "\.(png|gif|jpg|swf|css|js)(\?.*|)$") {
+        unset beresp.http.set-cookie;
+    }
+
     return(deliver);
+
 }
 
 sub vcl_deliver {
-  # This just inserts a diagnostic header to let us know if the content
-  # was served via a cache hit, or whether it was a miss.
 
-  if (obj.hits > 0) {
-    set resp.http.X-Cache = "HIT";
-  } else {
-   set resp.http.X-Cache = "MISS";
-  } 
-
-    return (deliver);
+    # Add debugging headers to cache requests
+    if (obj.hits > 0) {
+        set resp.http.X-Cache = "HIT";
+    }
+    else {
+        set resp.http.X-Cache = "MISS";
+    }
 }
 
-sub vcl_error {
-    set obj.http.Content-Type = "text/html; charset=utf-8";
-    synthetic {"
-<?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
- "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
-<html>
-  <head>
-    <title>"} obj.status " " obj.response {"</title>
-  </head>
-  <body>
-    <h1>Error "} obj.status " " obj.response {"</h1>
-    <p>"} obj.response {"</p>
-    <h3>Guru Meditation:</h3>
-    <p>XID: "} req.xid {"</p>
-    <hr>
-    <address>
-       <a href="http://www.varnish-cache.org/">Varnish cache server</a>
-    </address>
-
-</body>
-</html>
-"};
-    return (deliver);
+sub vcl_purge {
+    # Only handle actual PURGE requests, all the rest is discarded
+    if (req.method != "PURGE") {
+        # restart request
+        set req.http.X-Purge = "Yes";
+        return(restart);
+    }
 }
   '';
 }
