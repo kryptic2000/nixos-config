9a7affcbf4958a922077f0b31e561bfd4df56801
adam <adam@persbrandt.emtorp.se>
2021-03-25 10:46:58 +0100

    remake

--- services/mysql.nix
+++ services/mysql.nix
@@ -5,10 +5,13 @@
   networking.firewall.allowedUDPPorts = [ 3306 ];
   users.users.mysql = { };
 
-  services.mysql.enable = true;
-  services.mysql.dataDir = "/var/lib/mysql";
-  services.mysql.user = "mysql";
-  services.mysql.package = pkgs.mysql80;
-  services.mysql.port = 3306;
-  services.mysql.bind = "0.0.0.0";
+  services.mysql = {
+    enable = true;
+    package = pkgs.mysql80;
+    dataDir = "/var/lib/mysql";
+    initialScript = pkgs.writeText "initScript" ''
+      CREATE USER 'admin'@'%' IDENTIFIED BY 'admin';
+      GRANT ALL PRIVILEGES ON * . * TO 'admin'@'%';
+    '';
+  };
 }
