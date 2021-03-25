08db71f05b37bae3f32d32d4aabb42735b111380
adam <adam@persbrandt.emtorp.se>
2021-03-25 09:36:43 +0100

    added mysql user

--- services/mysql.nix
+++ services/mysql.nix
@@ -3,8 +3,10 @@
 {
   networking.firewall.allowedTCPPorts = [ 3306 ];
   networking.firewall.allowedUDPPorts = [ 3306 ];
+  users.users.mysql = { };
 
   services.mysql.enable = true;
+  services.mysql.user = "mysql";
   services.mysql.package = pkgs.mysql80;
   services.mysql.port = 3306;
   services.mysql.bind = "0.0.0.0";
