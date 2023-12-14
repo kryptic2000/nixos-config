bc66b1e877dc45dc997effda84d2d9cc4133eabd
adam <adam@persbrandt.emtorp.se>
2023-12-14 16:44:51 +0100

    new server

---
+++ servers/emtorp.se/mta01.nix
@@ -0,0 +1,30 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ../../../hardware-configuration.nix
+      ../../config/base23.nix
+      ../../config/users.nix
+      ../../services/networking23.nix
+      #../../config/emtorp.net/smtp.nix
+    ];
+
+  # Use the GRUB 2 boot loader.
+  boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
+
+  netcfg.ip4 = "91.228.90.92/28";
+  netcfg.gw4 = "91.228.90.81";
+
+  netcfg.addr4 = "91.228.90.92";
+  netcfg.addr6 = "2001:67c:22fc:100::92";
+
+  networking.enableIPv6 = true;
+  netcfg.ip6 = "2001:67c:22fc:100::92/64";
+  netcfg.gw6 = "2001:67c:22fc:100::1";
+
+  netcfg.iface = "ens33";
+  netcfg.hostName = "mta01";
+  netcfg.domain = "emtorp.se";
+}
+
