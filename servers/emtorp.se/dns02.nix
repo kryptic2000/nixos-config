c6e16b75bebc75ea696ba8e6273ac103c8d65707
kryptic2000 <adam@emtorp.se>
2024-01-02 11:48:57 +0000

    changed dns04

--- servers/emtorp.se/dns02.nix
+++ servers/emtorp.se/dns02.nix
@@ -4,25 +4,24 @@
   imports =
     [ # Include the results of the hardware scan.
       ../../../hardware-configuration.nix
-      ../../config/base.nix
+      ../../config/base23.nix
       ../../config/users.nix
-      ../../services/networking.nix
+      ../../services/networking23.nix
       ../../config/dns_resolver_unbound.nix
     ];
 
   # Use the GRUB 2 boot loader.
   boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
 
-  netcfg.ip4 = "91.228.90.93/28";
+
+  netcfg.ip4 = "91.228.90.93";
   netcfg.gw4 = "91.228.90.81";
 
-  networking.enableIPv6 = true;
-  netcfg.ip6 = "2001:67c:22fc:1::93/64";
-  netcfg.gw6 = "2001:67c:22fc:1::1";
+  networking.enableIPv6 = true
+  netcfg.ip6 = "2001:67c:22fc:2::93";
+  netcfg.gw6 = "2001:67c:22fc:2::1";
 
-  netcfg.iface = "enp0s3";
-  netcfg.hostName = "dns04.emtorp.net";
+  netcfg.iface = "ens33"; netcfg.hostName = "dns02.emtorp.se";
 
   netcfg.ns = [ "8.8.4.4" "8.8.8.8" ];
-
 }
