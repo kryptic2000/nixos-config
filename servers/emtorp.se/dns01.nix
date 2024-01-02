c41b6d811d2c1a737f5ae07e449ac981d76a82d7
kryptic2000 <adam@emtorp.se>
2024-01-02 11:59:15 +0000

    update to nixos23

--- servers/emtorp.se/dns01.nix
+++ servers/emtorp.se/dns01.nix
@@ -14,15 +14,16 @@
   boot.loader.grub.device = "/dev/sda"; # or "nodev" for efi only
 
 
-  netcfg.ip4 = "91.228.90.133";
+  netcfg.ip4 = "91.228.90.133/28";
   netcfg.gw4 = "91.228.90.129";
 
   networking.enableIPv6 = true
-  netcfg.ip6 = "2001:67c:22fc:1::133";
+  netcfg.ip6 = "2001:67c:22fc:1::133/64";
   netcfg.gw6 = "2001:67c:22fc:1::1";
 
   netcfg.iface = "ens33";
-  netcfg.hostName = "dns01.emtorp.se";
+  netcfg.hostName = "dns01";
+  netcfg.domain = "emtorp.se";
 
   netcfg.ns = [ "8.8.4.4" "8.8.8.8" ];
 }
