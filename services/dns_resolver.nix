c3fff478bfa208d159383cbeee83ef194e3a5382
kryptic2000 <adam@emtorp.se>
2024-01-02 12:09:45 +0000

    update syntax

--- services/dns_resolver.nix
+++ services/dns_resolver.nix
@@ -10,8 +10,7 @@
     settings = {
       access-control = [ "127.0.0.0/24 allow" "91.228.90.0/24 allow" "2001:67c:22fc::/48 allow" "10.5.1.0/24 allow"];
       interface = [ "0.0.0.0" "::0"];
-
-    }
+    };
     #forward-zone = [{
     #  name
     #}];
