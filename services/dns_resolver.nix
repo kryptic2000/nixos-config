08e323f6c5285ec77960937383cef390cddcbd19
kryptic2000 <adam@emtorp.se>
2024-01-02 12:08:50 +0000

    update syntax

--- services/dns_resolver.nix
+++ services/dns_resolver.nix
@@ -7,8 +7,11 @@
 
   services.unbound = {
     enable = true;
-    allowedAccess = [ "127.0.0.0/24" "91.228.90.0/24" "2001:67c:22fc::/48" "10.5.1.0/24"];
-    interfaces = [ "0.0.0.0" "::0"];
+    settings = {
+      access-control = [ "127.0.0.0/24 allow" "91.228.90.0/24 allow" "2001:67c:22fc::/48 allow" "10.5.1.0/24 allow"];
+      interface = [ "0.0.0.0" "::0"];
+
+    }
     #forward-zone = [{
     #  name
     #}];
